1164355883c3b0a94381427fe3841f56ae77e46f
Ahmad <ahmadalkali.dev@gmail.com>
2026-01-05 17:28:07 +0100

    Setup tests on the server

---
+++ server/jest.config.js
@@ -0,0 +1,35 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>'],
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'service/**/*.ts',
+    'controllers/**/*.ts',
+    'utils/**/*.ts',
+    '!**/node_modules/**',
+    '!**/tests/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testTimeout: 10000,
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        module: 'ES2020',
+      },
+    }],
+  },
+};
